26382c0b5ffa1fb829d32e347c4ba012a5046fc3
kerighan <max.chbx@gmail.com>
2021-04-15 16:46:25 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name="convectors",
+    version="0.0.0",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy", "scikit-learn", "scipy",
+        "tqdm", "pandas", "dill"]
+)
