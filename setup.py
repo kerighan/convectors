34a316a72546a24f935abcf46c0dc26bdc4bc7df
kerighan <max.chbx@gmail.com>
2021-04-15 16:55:37 +0200

    prettify setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         "numpy", "scikit-learn", "scipy",
-        "tqdm", "pandas", "dill"]
+        "tqdm", "pandas", "dill"
+    ]
 )
