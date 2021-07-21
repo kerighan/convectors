4fc846d751861d146ffb8c3aee2b67b219ba187c
Maixent Chenebaux <max.chbx@gmail.com>
2021-07-21 12:23:11 +0200

    put Lemmatizer in layers

--- convectors/layers.py
+++ convectors/layers.py
@@ -1,7 +1,8 @@
 from .classifier import (MLP, SVM, AdaBoost, Keras, RandomForest, Transformer,
                          XGBoost)
 from .embedding import CountVectorizer, Doc2Vec, OneHot, Sequence, TfIdf
-from .linguistics import Contract, FindAll, Phrase, Snowball, Sub, Tokenize
+from .linguistics import (Contract, FindAll, Lemmatizer, Phrase, Snowball, Sub,
+                          Tokenize)
 from .multi import Merge
 from .preprocessing import Normalize
 from .reduction import NMF, PCA, SVD, UMAP
@@ -10,7 +11,7 @@ from .special import SplitHashtag
 __all__ = [
     "Tokenize", "Snowball", "Phrase", "Sub", "Contract", "FindAll",
     "TfIdf", "CountVectorizer", "Sequence", "OneHot", "Doc2Vec",
-    "SVD", "PCA", "NMF", "UMAP",
+    "SVD", "PCA", "NMF", "UMAP", "Lemmatizer",
     "RandomForest", "AdaBoost", "SVM", "MLP", "Keras", "Transformer",
     "XGBoost", "Merge", "Normalize", "SplitHashtag"
 ]
