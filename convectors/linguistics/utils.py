03b423b6faa4181e98193010ac9e9997d4837ba7
Maixent Chenebaux <max.chbx@gmail.com>
2022-12-08 12:31:44 +0100

    updated fr_ner.p

--- convectors/linguistics/utils.py
+++ convectors/linguistics/utils.py
@@ -8,6 +8,10 @@ name2regex = {
                         u"\U0001F680-\U0001F6FF"  # transport & map symbols
                         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
                         "])", flags=re.UNICODE),
+    "flag": re.compile(
+        u'(\U0001F1F2\U0001F1F4)|'       # Macau flag
+        u'([\U0001F1E6-\U0001F1FF]{2})'  # flags
+        "+", flags=re.UNICODE),
     "hashtag": r"(\#[a-zA-Z0-9_]+\b)",
     "mention": r"(\@[a-zA-Z0-9_]+\b)",
     "email": r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",
