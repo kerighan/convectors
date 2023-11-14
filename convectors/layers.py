7f5daf5819a86df0e12e3153dd76a290ba70485e
kerighan <max.chbx@gmail.com>
2023-11-14 18:15:23 +0100

    updated tokenmonster in layers list

--- convectors/layers.py
+++ convectors/layers.py
@@ -1,8 +1,7 @@
 from .base_layer import Input
 from .merging import Concatenate
-from .preprocessing import (
-    Prefix, Suffix, Pad, DocumentSplitter, OneHot, Sub, FindAll)
-from .tokenizers import Tokenize, SnowballStem, Tiktokenize
+from .preprocessing import Prefix, Suffix, Pad, DocumentSplitter, OneHot, Sub, FindAll
+from .tokenizers import Tokenize, SnowballStem, Tiktokenize, TokenMonster
 from .vectorizers import TfIdf, CountVectorizer, HashingVectorizer, BM25
 from .reduction import SVD
 from .operations import Lambda
@@ -10,10 +9,24 @@ from .keras import Keras
 
 
 __all__ = [
-    "Input", "Tokenize", "SnowballStem", "Tiktokenize",
-    "Prefix", "Suffix", "Concatenate", "Pad", "DocumentSplitter",
-    "FindAll", "OneHot", "Sub", "TfIdf", "CountVectorizer",
-    "HashingVectorizer", "BM25",
-    "SVD", "Lambda",
-    "Keras"
+    "Input",
+    "Tokenize",
+    "SnowballStem",
+    "Tiktokenize",
+    "TokenMonster",
+    "Prefix",
+    "Suffix",
+    "Concatenate",
+    "Pad",
+    "DocumentSplitter",
+    "FindAll",
+    "OneHot",
+    "Sub",
+    "TfIdf",
+    "CountVectorizer",
+    "HashingVectorizer",
+    "BM25",
+    "SVD",
+    "Lambda",
+    "Keras",
 ]
