259f9b502bc0222a1ab52cf2cf3dbe1dccfb1b0a
kerighan <max.chbx@gmail.com>
2025-04-23 15:50:59 +0200

    updaded Keras layer

--- convectors/keras/base.py
+++ convectors/keras/base.py
@@ -7,7 +7,7 @@ import numpy as np
 def chunked(iterable, n):
     """Yield successive n-sized chunks from iterable."""
     for i in range(0, len(iterable), n):
-        yield iterable[i:i + n]
+        yield iterable[i : i + n]
 
 
 class Keras(Layer):
@@ -28,14 +28,12 @@ class Keras(Layer):
         self._is_tflite_model = isinstance(model, bytes)
         if self._is_tflite_model:
             import tensorflow as tf
+
             self._interpreter = tf.lite.Interpreter(model_content=model)
             self._interpreter.allocate_tensors()
-            self._input_details = (
-                self._interpreter.get_input_details()[0]["index"])
-            self._inp_dtype = (
-                self._interpreter.get_input_details()[0]["dtype"])
-            self._output_details = (
-                self._interpreter.get_output_details()[0]["index"])
+            self._input_details = self._interpreter.get_input_details()[0]["index"]
+            self._inp_dtype = self._interpreter.get_input_details()[0]["dtype"]
+            self._output_details = self._interpreter.get_output_details()[0]["index"]
 
     def _unload(self):
         if not self._is_tflite_model:
@@ -48,37 +46,39 @@ class Keras(Layer):
     def _reload(self, custom_objects=None, **_):
         if not self._is_tflite_model:
             from tensorflow.keras import models
+
             try:
                 model = models.Model.from_config(
-                    self.config, custom_objects=custom_objects)
+                    self.config, custom_objects=custom_objects
+                )
             except KeyError:
                 model = models.Sequential.from_config(
-                    self.config, custom_objects=custom_objects)
+                    self.config, custom_objects=custom_objects
+                )
             model.set_weights(self.weights)
             del self.weights
             del self.config
             self._model = model
         else:
             import tensorflow as tf
+
             self._interpreter = tf.lite.Interpreter(model_content=self._model)
             self._interpreter.allocate_tensors()
-            self._input_details = (
-                self._interpreter.get_input_details()[0]["index"])
-            self._inp_dtype = (
-                self._interpreter.get_input_details()[0]["dtype"])
-            self._output_details = (
-                self._interpreter.get_output_details()[0]["index"])
+            self._input_details = self._interpreter.get_input_details()[0]["index"]
+            self._inp_dtype = self._interpreter.get_input_details()[0]["dtype"]
+            self._output_details = self._interpreter.get_output_details()[0]["index"]
 
     def _predict_tflite(self, X):
         output_tensors = []
+        if not isinstance(X, np.ndarray):
+            X = np.array(X)
         X_shape = list(X.shape)
         last_batch_size = -1
         for chunk in chunked(X, self._batch_size):
             # Resize the input tensor if necessary
             if last_batch_size != len(chunk):
                 X_shape[0] = len(chunk)
-                self._interpreter.resize_tensor_input(
-                    self._input_details, (X_shape))
+                self._interpreter.resize_tensor_input(self._input_details, (X_shape))
                 last_batch_size = len(chunk)
                 self._interpreter.allocate_tensors()
 
@@ -96,6 +96,7 @@ class Keras(Layer):
         # convert to matrix
         if not isinstance(series, np.ndarray):
             from scipy.sparse import issparse
+
             X = to_matrix(series)
             if issparse(X):
                 X = np.array(X.todense())
@@ -105,6 +106,7 @@ class Keras(Layer):
         # predict using either tflite of standard keras model
         if not self._is_tflite_model:
             return self._model.predict(
-                X, batch_size=self._batch_size, verbose=False, **self.kwargs)
+                X, batch_size=self._batch_size, verbose=False, **self.kwargs
+            )
         else:
             return self._predict_tflite(X)
